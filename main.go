29d78c89903c58155eb5edb208e464902e58a709
Admin <jmwl1229@52ig.cn>
2023-12-04 19:03:17 +0800

    设置gin为生产环境、优化部分功能

--- main.go
+++ main.go
@@ -21,20 +21,30 @@ type UrlItemModel struct {
 var Urls = make(map[string]UrlItemModel)
 
 func main() {
+	// 获取yaml配置
 	config.Config.GetConfig()
-	r := gin.Default()
+
+	// 将gin设置为生产环境模式
 	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	// 嗅探
 	r.GET("/xt", router)
+
+	fmt.Println("API:", fmt.Sprintf("http://127.0.0.1:%d/xt?url=", config.Config.Port))
 	err := r.Run(fmt.Sprintf(":%d", config.Config.Port))
 	if err != nil {
+		fmt.Println("启动失败", err)
 		return
 	}
 }
+
+// router 路由
 func router(c *gin.Context) {
 	url := c.Query("url")
 	url = strings.TrimSpace(url)
 	if len(url) == 0 {
-		c.JSON(200, gin.H{"code": 404, "msg": "解析失败"})
+		c.JSON(200, gin.H{"code": 404, "msg": "缺少URL"})
 		return
 	}
 	fmt.Println(getMD5(url))
@@ -107,6 +117,8 @@ func toParse(url string, c *gin.Context) {
 		"url":  "",
 	})
 }
+
+// getMD5 获取md5值
 func getMD5(text string) string {
 	// 创建一个MD5哈希对象
 	hash := md5.New()
